c08f48a67b93f7867a10f67aca8291827e0d8960
khaudio <kylehughes1@gmail.com>
2024-12-09 18:52:10 -0600

    re write

--- __main__.py
+++ __main__.py
@@ -1,46 +1,33 @@
 #!/usr/bin/env python
 
-import datetime
-import sys
-import json
+import random
 
 
-def open_file_as_text(filename: str):
-    with open(filename, 'r', encoding='utf-8') as f:
-        for line in f:
-            yield line
+def receive():
+    if (random.randint(0, 1)):
+        return 'recache'
+    else:
+        return 'recall'
 
 
-def timestamp_to_datetime(timestamp: str):
-    if not timestamp:
-        return
-    return datetime.datetime.strptime(
-            timestamp.replace('\u202f', ' '),
-            '%m/%d/%Y %I:%M %p'
-        )
+def recall(rc):
+    return (rc == 'recall')
 
 
-def process_csv_as_text(filepath):
-    timestamp, buff = '', ''
-    for index, line in enumerate(open_file_as_text(filepath)):
-        if '\u202f' in line:
-            yield index - 1, timestamp_to_datetime(timestamp), buff
-            timestamp = line.split(',')[0]
-            buff = line.lstrip(timestamp).lstrip(',')
-        else:
-            buff += line
-    yield index - 1, timestamp_to_datetime(timestamp), buff
+def proceed(mem, counter):
+    print(f'Proceeding with {mem} mem after {counter} loops')
+
 
 def main():
-    processor = process_csv_as_text(sys.argv[1])
-    _ = next(processor) # Skip column labels
-    data = dict()
-    for index, timestamp, message in processor:
-        data[index] = [timestamp.strftime('%Y-%m-%d %H:%M'), message]
-        print(index, timestamp, message)
-    json.dump(data, open('data.json', 'w'), indent=4)
+    mem = None
+    counter = 0
+    rc = receive()
+    while not mem:
+        rc = receive()
+        mem = (rc != 'recall')
+        counter += 1
+    proceed(mem, counter)
 
 
 if __name__ == '__main__':
     main()
-
